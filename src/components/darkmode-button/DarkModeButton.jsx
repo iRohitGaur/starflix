2a8b2f5e92dbd3931be9fdbc447b08b9b77bedd7
Rohit Gaur <knotjinx@gmail.com>
2022-03-25 17:20:06 +0530

    setup theme

---
+++ src/components/darkmode-button/DarkModeButton.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { NotoV1Owl, NotoChicken } from "../../assets/Icons";
+
+const preferDarkQuery = "(prefers-color-scheme: dark)";
+
+function DarkModeButton() {
+  // RG: PERF: lazy state initialization
+  // RG: Check if user has a prefered mode set in localStorage.
+  // If not then check for system preference match and provide the mode
+  const [theme, setTheme] = useState(
+    () =>
+      localStorage.getItem("sui-mode") ??
+      (window.matchMedia(preferDarkQuery).matches ? "dark" : "light")
+  );
+
+  // RG: Every time the "theme" changes, update the document and localStorage
+  useEffect(() => {
+    document.documentElement.setAttribute("sui-mode", theme);
+    localStorage.setItem("sui-mode", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <button className="btn_icon_fa" onClick={toggleTheme}>
+      {theme === "light" && <NotoV1Owl />}
+      {theme === "dark" && <NotoChicken />}
+    </button>
+  );
+}
+
+export default DarkModeButton;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as DarkModeButton } from "./darkmode-button/DarkModeButton";
